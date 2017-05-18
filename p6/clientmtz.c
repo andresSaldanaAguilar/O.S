77239803a2cae29d072a67a01115e171cfe44b74
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2017-05-17 22:51:53 -0500

    Trabajando en comunicacion en memoria compartida

--- p6/clientmtz.c
+++ p6/clientmtz.c
@@ -11,7 +11,7 @@
 #include <sys/shm.h> /* shm*  */
 
 #define FILEKEY "/bin/cat"
-#define KEY 1301
+
 #define MAXBUF 300
 
 int main (void) {
@@ -39,65 +39,119 @@ int main (void) {
                    {0,1,2,3,3,2,1,0,1,2},
                    {0,1,2,3,3,2,1,0,1,5}};
 
-   /* Key to shared memory */
-   int key = ftok(FILEKEY, KEY);
-   if (key == -1) {
-      fprintf (stderr, "Error en la llave.\n");
+   /* Llaves de memoria compartida*/
+   int KEY[4]={1301,1302,1303,1304};
+   int key[4],zona_id[4];
+   int *buffer[4];
+
+   /*creamos los 4 buffers necesarios*/
+   for(int i=0;i<4;i++){
+   key[i] = ftok(FILEKEY, KEY[i]);
+   if (key[i] == -1) {
+      fprintf (stderr, "Error en la llave %i.\n",i);
       return -1;
    }
 
-   /* we create the shared memory */
-   int zona_id = shmget (key, sizeof(int)*MAXBUF, 0777 | IPC_CREAT);
-   if (zona_id == -1) {
-      fprintf (stderr, "Error en la zona_id.\n");
+   /*Creamos la memoria compartida*/
+   zona_id[i] = shmget (key[i], sizeof(int)*MAXBUF, 0777 | IPC_CREAT);
+   if (zona_id[i] == -1) {
+      fprintf (stderr, "Error en la zona_id. %i\n",i);
       return -1;
    }
 
-   printf ("Zona ID de la memoria compartida: %i\n", zona_id);
+   printf ("Zona ID de la memoria compartida: %i --> %i\n", zona_id[i],i);
 
-   int *buffer; /* shared buffer */
-   /* we declared to zone to share */
-   buffer = shmat (zona_id, (char *)0, 0);
-   if (buffer == NULL) {
+   /*declaramos zona a compartir*/
+   buffer[i] = shmat (zona_id[i], (char *)0, 0);
+   if (buffer[i] == NULL) {
       fprintf (stderr, "Error reservando memoria.\n");
       return -1;
    }
-
-
-   printf ("Apuntador a buffer: %p\n", buffer);
-
+   printf ("Apuntador a buffer: %p --> %i\n", buffer[i],i);
+   }
 
 
   /*empezamos con el manejo de los procesos*/
 
   /*procedemos a llenar el buffer con las matrices*/
-  int contador=0;
+  int contador_padre=0;
   for(int i=0;i<10;i++){
     for (int j = 0; j < 10; j++){
-       buffer[contador] = m1[i][j];
-       contador++;
+       buffer[1][contador_padre] = m1[i][j];
+       contador_padre++;
     }
   }
-  //printf("%i\n",contador);
+  printf("%i\n",contador_padre);
   /*indice final de m1*/
   for(int k=0;k<10;k++){
     for (int l = 0; l < 10; l++){
-       buffer[contador] = m2[k][l];
-       contador++;
+       buffer[1][contador_padre] = m2[k][l];
+       contador_padre++;
     }
   }
-  //printf("%i\n",contador);
+  printf("%i\n",contador_padre);
   /*indice final de m2*/
 
   int idproc;
   if((idproc=fork())==0){
-    for(int i=0;i<200;i++){
-    printf("%i",buffer[i]);
+
+    int m1h1 [10][10], m2h1 [10][10], msuma [10][10];
+    int aux=0;
+    /*llenamos las matrices y efectuamos suma*/
+    for(int i=0;i<10;i++){
+      for (int j = 0; j < 10; j++){
+           m1h1[i][j]=buffer[1][aux];
+           aux++;
+      }
     }
+    for(int l=0;l<10;l++){
+      for (int m = 0; m < 10; m++){
+           m2h1[l][m]=buffer[1][aux];
+           aux++;
+      }
+    }
+    /*suma*/
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+          msuma[i][j]=m1h1[i][j]*m2h1[i][j];
+      }
+    }
+    /*impresion*/
+    printf("Matriz 1:\n");
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+          printf("%i ",m1h1[i][j]);
+          if(j==9){
+            printf("\n");
+          }
+      }
+    }
+    /*impresion*/
+    printf("Matriz 2:\n");
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+          printf("%i ",m2h1[i][j]);
+          if(j==9){
+            printf("\n");
+          }
+      }
+    }
+    /*impresion*/
+    printf("Matriz suma:\n");
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+          printf("%i ",msuma[i][j]);
+          if(j==9){
+            printf("\n");
+          }
+      }
+    }
+
+
     /*escritura en buffer*/
-    if((idproc=fork())==0){
+    //if((idproc=fork())==0){
       /*lectura en buffer*/
-    }
+    //}
     exit(0);
   }
   wait(0);
